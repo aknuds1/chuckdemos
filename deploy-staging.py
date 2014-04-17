56ab004631eb664f9aae02a01d8e416aa7e78080
Arve Knudsen <arve.knudsen@gmail.com>
2014-04-17 13:51:19 +0200

    Add stage deployment script

---
+++ deploy-staging.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import subprocess
+import os.path
+
+dpath = os.path.dirname(__file__)
+if dpath:
+  os.chdir(dpath)
+
+if not os.path.exists('public'):
+  os.makedirs('public')
+robots = None
+try:
+  with open('public/robots.txt', 'wb') as robots:
+    robots.write("""User-agent: *
+Disallow: /
+""")
+
+  subprocess.check_call(["meteor", "deploy", 'chuckdemos.meteor.com'])
+finally:
+  if robots is not None:
+    os.remove(robots.name)
