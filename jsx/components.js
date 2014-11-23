5ac8112fe572e84c2dc0f1b3aa8cab5708c54d2a
Arve Knudsen <arve.knudsen@gmail.com>
2014-11-23 23:21:15 +0100

    Add missing file

---
+++ jsx/components.js
@@ -0,0 +1,10 @@
+'use strict'
+
+var SoundIcon = React.createClass({
+  render: function () {
+    return (
+      <i className="fa fa-headphones slightly-transparent" data-toggle="tooltip" data-placement="right"
+      title="" data-original-title="Generates sound"></i>
+    )
+  },
+})
