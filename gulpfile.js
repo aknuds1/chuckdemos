adb9c3c31ac9649e100efae8e59ef0735ba6ed0e
Arve Knudsen <arve.knudsen@gmail.com>
2014-11-23 23:48:07 +0100

    Integrate ChucK

--- gulpfile.js
+++ gulpfile.js
@@ -8,9 +8,11 @@ var react = require('gulp-react')
 var watch = require('gulp-watch')
 var plumber = require('gulp-plumber')
 var jshint = require('gulp-jshint')
+var shell = require('gulp-shell')
 
 var bowerDir = './bower_components/' 
 var sassSrcSpec = ['./styles/*.scss']
+var jsLibSrcSpec = ['./lib/*.js']
 var jsxSrcSpec = [
   './jsx/components.js',
   './jsx/about.js',
@@ -29,20 +31,27 @@ function defaultBuild() {
         ]
      }))
     .pipe(gulp.dest('./public/css'))
+
+  var jsLibFiles = gulp.src(jsLibSrcSpec)
+    .pipe(plumber())
+    .pipe(gulp.dest('./public/js'))
+
   var jsxFilesSrc = gulp.src(jsxSrcSpec)
     .pipe(plumber())
     .pipe(react())
   jsxFilesSrc
     .pipe(jshint())
     .pipe(jshint.reporter('default'))
-
   var jsxFiles = jsxFilesSrc
     .pipe(gulp.dest('./public/js'))
+
   var bowerJs = gulp.src(bowerFiles(), {read: false})
+
   return gulp.src(htmlSrcSpec)
     .pipe(plumber())
     .pipe(inject(css))
     .pipe(inject(bowerJs, {name: 'bower'}))
+    .pipe(inject(jsLibFiles, {name: 'lib'}))
     .pipe(inject(jsxFiles, {name: 'jsx'}))
     .pipe(gulp.dest('./public/html'))
 }
@@ -54,7 +63,13 @@ gulp.task('bower', function () {
 gulp.task('default', defaultBuild)
 
 gulp.task('watch', function () {
-  watch(sassSrcSpec.concat(jsxSrcSpec).concat(htmlSrcSpec), function () {
+  watch(sassSrcSpec.concat(jsxSrcSpec).concat(htmlSrcSpec).concat(jsLibSrcSpec), function () {
     return defaultBuild()
   })
+})
+
+gulp.task('chuck', function () {
+  shell.task(['cd chuck/src && make -j5 emscripten'])
+  return gulp.src('./chuck/src/chuck.js*')
+    .pipe(gulp.dest('./lib/'))
 })
